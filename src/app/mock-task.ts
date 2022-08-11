e52768029f50b6b0e0438e0a02de76975c46b2c4
NDeleo94 <n.deleo94@gmail.com>
2022-08-11 12:42:26 -0300

    Task List Added

---
+++ src/app/mock-task.ts
@@ -0,0 +1,27 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Hacer las compras',
+    day: '2020',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Hacer las compras 2',
+    day: '2020',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Hacer las compras 3 ',
+    day: '2020',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Hacer las compras 4',
+    day: '2020',
+    reminder: true,
+  },
+];
