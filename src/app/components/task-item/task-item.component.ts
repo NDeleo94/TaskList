e6b023963ddb3176e3d6b12b4a2871587d1f1514
NDeleo94 <n.deleo94@gmail.com>
2022-08-11 12:50:05 -0300

    fontawesome added

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TASKS } from 'src/app/mock-task';
 import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -9,6 +10,7 @@ import { Task } from 'src/app/Task';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task: Task = TASKS[0];
+  faTimes = faTimes;
 
   constructor() {}
 
