b34a6ec34db03518c3e7eaf1af76b29771dac034
NDeleo94 <n.deleo94@gmail.com>
2022-08-11 22:58:59 -0300

    form add task added

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -26,4 +26,8 @@ export class TasksComponent implements OnInit {
     task.reminder = !task.reminder;
     this.taskService.updateReminderTask(task).subscribe();
   }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
 }
